5d21cae8eaf7318787adc66363990db7a96bc673
Christoph Bühler <christoph@smartive.ch>
2016-03-31 19:00:47 +0200

    adding mock classes

--- RestExtractor.spec.ts
+++ RestExtractor.spec.ts
@@ -3,10 +3,52 @@ import asPromised = require('chai-as-promised');
 import sinon = require('sinon');
 import sinonChai = require('sinon-chai');
 import {RestExtractor} from './RestExtractor';
+import {EventEmitter} from 'events';
 
 let should = chai.should();
 chai.use(asPromised);
 chai.use(sinonChai);
+
+abstract class Mock {
+    public request(url:string, opt:any):EventEmitter {
+        let emitter = new EventEmitter();
+        setTimeout(() => {
+            try {
+                let data = this.data();
+                emitter.emit('complete', data);
+            } catch (e) {
+                emitter.emit('error', e);
+            }
+        }, 100);
+        return emitter;
+    }
+
+    protected abstract data():any;
+}
+
+class ArrayMock extends Mock {
+    protected data():any {
+        return [
+            {objId: 1},
+            {objId: 2},
+            {objId: 3},
+            {objId: 4},
+            {objId: 5}
+        ];
+    }
+}
+
+class ObjectMock extends Mock {
+    protected data():any {
+        return {objId: 5};
+    }
+}
+
+class ErrorMock extends Mock {
+    protected data():any {
+        throw new Error('test');
+    }
+}
 
 describe('RestExtractor', () => {
 
