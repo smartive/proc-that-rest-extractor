c26724356d2b3291f4469b6dd75348987f4e56c6
Dominique Wirz <dominique.wirz@gmail.com>
2016-10-28 10:08:28 +0200

    Feature/add request timeout (#5)
    * add request timeout
    
    * update changelog
    
    * redesign
    
    * tescht fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Added
+- Option for request timeout
 
 
 ## [v0.4.0]
--- RestExtractor.spec.ts
+++ RestExtractor.spec.ts
@@ -179,4 +179,26 @@ describe('RestExtractor', () => {
             });
     });
 
+    it('should set request timeout', done => {
+        extractor = new RestExtractor('url', RestExtractorMethod.Get, undefined, {
+            timeout: 42
+        });
+        (extractor as any).rest = new SelectorMock();
+
+        sinon.spy((extractor as any).rest, 'request');
+
+        extractor
+            .read()
+            .subscribe(() => {}, err => {
+                done(err);
+            }, () => {
+                try {
+                    (extractor as any).rest.request.should.be.calledWith('url', { method: 'get', timeout: 42 });
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+    });
+
 });
